2e955e58c2a8a53ba71ab3a990d8e649f007e79c
tanmylien <tan.my.lien2002@gmail.com>
2025-02-16 10:37:22 -0800

    Updated API message

--- app/app.py
+++ app/app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello, World!"
+@app.route('/api/message')
+def api_message():
+    return {"team members": "My Lien Tan, Brandon Vo, Gian David Marquez, Cheyenne Chavis"}
 if __name__ == "__main__":
     app.run(debug=True)
